472c734328dfc7d6fbb999d0086f36b311550e67
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-09-24 21:13:06 +0600

    update

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                     if (have_posts()) :
                         while (have_posts()) : the_post();
                     ?>
-                            <?php echo get_template_part('template-parts/content'); ?>
+                            <?php echo get_template_part('template-parts/content', get_post_format());  ?>
                     <?php
                         endwhile;
                     endif;
